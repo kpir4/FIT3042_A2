46043c87e317e5ab40455e001c213be35d664821
kpir4 <kpir4@student.monash.edu>
2017-05-06 00:00:56 +1000

    hide_A2 has all function with good commenting. In fuction comments still required.

--- hide_A2.c
+++ hide_A2.c
@@ -22,7 +22,7 @@ int main(int argc, char **argv) {
 		int num_files = atoi(argv[2]);
 		if (num_files < 225) {
 			// finds the combined number of characters that can be hidden by all images
-			int total_image_cap = get_total_cap(argv, num_files);
+			int total_image_cap = get_total_cap(argv[3], num_files);
 			
 			if (total_image_cap == -1) {
 				printf("Error: Image format must be P6.\nTerminating...\n");
@@ -58,40 +58,41 @@ int main(int argc, char **argv) {
 	return 0;
 }
 
-/* Returns the number of characters that can be hidden within the
-** available input image files or -1 when at least one of the
-** input files are not a P6 format ppm image 
-*/
-int get_total_cap(char ** files, int num_files) {
+/* Find the total number of bits that can be hidden within the number of 
+ * .ppm files specified.
+ *
+ * filename: the name of the .ppm file to be used to hide the message. Files
+ *			 must have the same base name and only differ by a sequence number
+ *			 (i.e. filename-000.ppm, filename-001.ppm).
+ * num_files: the number of files that will be used to hide the message.
+ *
+ * returns: the number of bits that can be hidden within the number of specified
+ *			.ppm files.
+ */
+int get_total_cap(char * filename, int num_files) {
 	int total_file_cap = 0, file_cap, i;
 
 	for (i = 0; i < num_files; i++) {
-		char *inf_name = (char*)malloc(strlen(files[3]) + 10);
-		strcpy(inf_name, files[3]);
-		strcpy(inf_name, add_file_extension(inf_name, i));
-		FILE *inf = fopen(inf_name, "r");
-		if (inf == NULL) {
-			printf("Error: Could not open file.\n");
-			printf("Terminating...\n");
-			return -2;
-		}
+		/* Opens input image and moves file pointer to the width
+		property of the image */
+		FILE *inf = open_file(filename, i);
+		// The maximum number of bits that can be hidden by combined images
+		total_file_cap += get_file_cap(inf);				
 
-		if (correct_magic_num(inf)) {
-			ignore_comments(inf);
-			total_file_cap += get_file_cap(inf);				
-		}
-		else {
-			return -1;
-		}
 		fclose(inf);
-		free(inf_name);
 	}
 	return total_file_cap;
 }
 
-/* Returns the message length and updates the pass in message variable.
-** Uses input redirection if avaiable or prompts user for message if not.
-*/
+/* Dynamically allocates memory to store the message from stdin. If the
+ * memory original allocated is not enough, more memory is allocated til
+ * the whole message fits.
+ *
+ * message: a pointer to a character array to contain the message is passed
+ *			in to act at a pass by reference
+ *
+ * returns: the length of the message
+ */
 int get_msg(char **message) {
 	int max = 20;
 	*message = (char*)malloc(max);
@@ -117,7 +118,12 @@ int get_msg(char **message) {
 	}
 }
 
-
+/* Copies the header of a .ppm file to another file
+ * 
+ * inf: a file pointer to the .ppm whose header will be copied
+ * outf: a file pointer to the file which will contain the copied header
+ * num_header_lines: the number of lines within the header of .ppm file
+ */
 void copy_header(FILE *inf, FILE *outf, int num_header_lines) {
 	int i;
 	char temp;
@@ -136,9 +142,13 @@ void copy_header(FILE *inf, FILE *outf, int num_header_lines) {
 	}
 }
 
-/* Returns the number of line in the header of the input file. The input file
-** must be a .ppm image.
-*/
+/* Counts the number of newline characters in the file to determine
+ * how many lines are in the header of the .ppm file.
+ *
+ * inf: a file pointer to the .ppm file whose header will be counter.
+ *
+ * returns: the number of lines in the header of the input file
+ */
 int count_header_lines(FILE *inf) {
 	char temp;
 	int count = 0;
@@ -158,10 +168,17 @@ int count_header_lines(FILE *inf) {
 	return count;
 }
 
-/* Hide the message in the images. Returns 0 is message is successfully hidden or -1
-** if an error occurs.
-*/
-int hide_msg(char *inf_name, char *outf_name, char *msg, int msg_len, int num_files) {
+/* Hides a message in a .ppm file by using another .ppm file as the base.
+ * 
+ * inf_name: the base name of the input .ppm file (i.e. without -001.ppm).
+ * outf_name: the base name of the output .ppm file (i.e. without -001.ppm).
+ * msg: the message to be hidden within the output .ppm file.
+ * msg_len: the length of the message to be hidden.
+ * num_files: the number of files the message can potentially be hidden within.
+ *			  Not all of the files may be needed to hide the message, but all
+			  output copies will be produced regardless.
+ */
+void hide_msg(char *inf_name, char *outf_name, char *msg, int msg_len, int num_files) {
 	int i, curr_img = 0, bits_iter = 0;
 	char curr_char;
 	do {
@@ -191,15 +208,22 @@ int hide_msg(char *inf_name, char *outf_name, char *msg, int msg_len, int num_fi
 		fclose(outf);
 		curr_img++;
 	} while (curr_img < num_files);
-	return 0;
 }
 
-
-void encode_length(FILE *in, FILE *out, int length) {
+/* Hides the length of the message within the first 8 bytes of the .ppm file
+ * immediately after the header. Each bit of the message length is hidden in
+ * a byte of the .ppm file.
+ * 
+ * inf: a file pointer to the .ppm file which will used to get each byte to hide
+ *     each bit of the message length.
+ * outf: a file pointer to the output .ppm file which contains the hidden message.
+ * length: the length of the message which will be hidden in the output .ppm file
+ */
+void encode_length(FILE *inf, FILE *outf, int length) {
 	char temp;
 	int i, l;
 	for (i = 0; i < 8; i++) {
-		temp = fgetc(in);
+		temp = fgetc(inf);
 		l = length;
 		l >>= 7 - i;
 		if ((l & 1) == 1){
@@ -208,14 +232,24 @@ void encode_length(FILE *in, FILE *out, int length) {
 		else {
 			if ((temp & 1) == 1) temp--;
 		}
-		fputc(temp, out);
+		fputc(temp, outf);
 	}
 
 	return;
 }
 
 
-// Hides a single character within a .ppm image
+/* Hides a single bit from a byte of a message within a byte of a .ppm file.
+ *
+ * inf: a file pointer to the input .ppm file.
+ * outf: a file pointer to the output .ppm file which will contain the hidden message.
+ * msg: the message to be hidden within the output .ppm file.
+ * bit_shift: the number of right bitwise shifts required to hide the correct bit of
+ *			  the of character from the message.
+ * msg_len: the length of the message to be hidden.
+ * bits_iter: the number of bits that have been hidded. This counter actually goes beyond
+ *			  the number of bits hidden, but is ignored at that point.
+ */
 void hide_bit(FILE *inf, FILE *outf, char *msg, int bit_shift, int msg_len, int bits_iter){
 	unsigned char colour_chan;
 	char curr_bit;
@@ -249,7 +283,12 @@ void hide_bit(FILE *inf, FILE *outf, char *msg, int bit_shift, int msg_len, int 
 	fputc(colour_chan, outf);
 }
 
-
+/* Creates a number of child processes based on the number of message to be
+ * to be hidden in parallel.
+ * 
+ * file: the name of the file that contains the list of input and output files,
+ *		 as well as the messages to be hidden.
+ */
 void hide_fork(char *file) {
 	FILE *inf = fopen(file, "r");
 	char *msg_file, *inf_img, *outf_img, *msg;
@@ -275,7 +314,16 @@ void hide_fork(char *file) {
 	free(msg);
 }
 
-
+/* Retrieves a line line of parameters from a text file.
+ *
+ * inf: a file pointer to the text file that contains the parametres.
+ * msg_file: a pointer to a character array which acts as if passed by reference.
+ *			 The name of the file containing the message should be passed back.
+ * inf_img: a pointer to a character array which acts as if passed by reference.
+ *			The name of the input .ppm file should be passed back.
+ * outf_img: a pointer to a character array which acts as if passed by reference.
+ *			 The name of the output .ppm file should be passed back.
+ */
 void get_parametres(FILE *inf, char **msg_file, char **inf_img, char **outf_img) {
 	char temp;
 	int i = 0, str_len = 0;
@@ -296,9 +344,14 @@ void get_parametres(FILE *inf, char **msg_file, char **inf_img, char **outf_img)
 				strcpy(temp_str, "");
 				i++;
 			}
-			else {
+			else if (i == 2) {
 				temp_str[str_len] = 0;
 				strcpy(*outf_img, temp_str);
+				i++;
+			}
+			else {
+				printf("Error: File is in the wrong format. Only 3 entries permitted for each line.\nTerminating...\n");
+				exit(1);
 			}
 		}
 		if (str_len == max - 1) {
@@ -310,7 +363,12 @@ void get_parametres(FILE *inf, char **msg_file, char **inf_img, char **outf_img)
 	free(temp_str);
 }
 
-
+/* Retrieves a message found within a file.
+ * 
+ * msg_file: the name of the file containing the message.
+ *
+ * returns: the message contained within the message file.
+ */
 char *get_msg_from_file(char *msg_file) {
 	FILE *inf = fopen(msg_file, "r");
 	int max = 20;
@@ -333,7 +391,13 @@ char *get_msg_from_file(char *msg_file) {
 	return message;
 }
 
-
+/* Checks if there are more lines to retrieve data from.
+ * 
+ * inf: a file pointer to the file containing the data
+ *
+ * returns: 0 if the end of file has been reached or 1 if the
+ *			is still more data to read.
+ */
 int check_for_more(FILE *inf) {
 	char temp = fgetc(inf);
 	if (temp == EOF)
--- hide_A2.h
+++ hide_A2.h
@@ -9,7 +9,7 @@ int get_file_cap(FILE *inf);
 int get_msg(char **message);
 void copy_header(FILE *inf, FILE *outf, int num_header_lines);
 int count_header_lines(FILE *inf);
-int hide_msg(char *inf_name, char *outf_name, char *msg, int msg_len, int num_files);
+void hide_msg(char *inf_name, char *outf_name, char *msg, int msg_len, int num_files);
 void hide_bit(FILE *inf, FILE *outf, char *msg, int bit_shift, int msg_len, int bits_iter);
 void encode_length(FILE *in, FILE *out, int length);
 void hide_fork(char *file);
